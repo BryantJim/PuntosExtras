443a684c4abe5acf734a30673f730e33a58ab4a5
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-06 20:48:15 -0400

    Aqui se almacena las clases que se usan en el proyecto.

---
+++ PuntosExtras/Entidades/Personas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PuntosExtras.Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonasId { get; set; }
+        public String Nombres { get; set; }
+
+        public Personas()
+        {
+            PersonasId = 0;
+            Nombres = string.Empty;
+        }
+    }
+}
