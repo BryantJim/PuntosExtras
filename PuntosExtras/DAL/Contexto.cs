4f9d76712680b47f2fc3aef3208d24a638152bb2
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-06 21:12:33 -0400

    Conexión del proyecto con la base de datos

---
+++ PuntosExtras/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using PuntosExtras.Entidades;
+
+namespace PuntosExtras.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"DataBase\Personas.db");
+        }
+    }
+}
